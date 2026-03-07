2bf8b75b5a0cb241d7eca968ce7fe3d2775c7b37
mohdanas86 <anasalam7739@gmail.com>
2026-03-07 18:20:36 +0530

    add errorHandler function for centralized error handling

---
+++ src/errorHandler.js
@@ -0,0 +1,10 @@
+function errorHandler(err, req, res, next) {
+    const status = err.status || 500;
+
+    res.status(status).json({
+        success: false,
+        message: err.message || "Internal Server Error"
+    })
+}
+
+export default errorHandler;
