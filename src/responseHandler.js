b666849b504356e17564bd0368b4c073fba05987
mohdanas86 <anasalam7739@gmail.com>
2026-03-07 18:20:41 +0530

    add responseHandler for standardized success and error responses

---
+++ src/responseHandler.js
@@ -0,0 +1,20 @@
+function responseHandler(req, res, next) {
+    res.success = function (data, message = "success") {
+        res.status(200).json({
+            success: true,
+            message,
+            data
+        });
+    }
+
+    res.error = function (message = "Error", status = 500) {
+        res.status(status).json({
+            success: false,
+            message
+        })
+    }
+
+    next();
+}
+
+export default responseHandler;
